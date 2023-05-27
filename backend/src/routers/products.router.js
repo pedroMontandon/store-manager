d535126d69b6b04a032b8fd9c7983ac601fbb1ee
pedroMontandon <pedrobarbosa89@gmail.com>
2023-05-26 21:17:41 -0300

    4 feita

--- backend/src/routers/products.router.js
+++ backend/src/routers/products.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const { validateNewProduct } = require('../middlewares/validateProducts');
 
 const router = express.Router();
 
 router.get('/', productsController.getAllProducts);
 router.get('/:id', productsController.findProductById);
-router.post('/', productsController.createProduct);
+router.post('/', validateNewProduct, productsController.createProduct);
 
 module.exports = router;
--- backend/tests/unit/controllers/products.controller.test.js
+++ backend/tests/unit/controllers/products.controller.test.js
@@ -10,68 +10,101 @@ const productsController = require('../../../src/controllers/products.controller
 const { allProducts, newProduct } = require('../../mocks/productsMock');
 
 describe('Teste de unidade do controller Products', function () {
-    afterEach(function () {
-        sinon.restore();
-    });
-    it('Recuperar todos os produtos', async function () {
-        const req = {};
-        const res = {};
+  afterEach(function () {
+      sinon.restore();
+  });
+  it('Recuperar todos os produtos', async function () {
+      const req = {};
+      const res = {};
 
-        res.status = sinon.stub().returns(res);
-        res.json = sinon.stub().returns(res);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
 
-        sinon.stub(productsService, 'getAllProducts').resolves({ type: 200, data: allProducts });
-        
-        await productsController.getAllProducts(req, res);
+      sinon.stub(productsService, 'getAllProducts').resolves({ type: 200, data: allProducts });
+      
+      await productsController.getAllProducts(req, res);
 
-        expect(res.status).to.have.been.calledWith(200);
-        expect(res.json).to.have.been.calledWith(allProducts);
-    });
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allProducts);
+  });
 
-    it('Recuperar um produto pelo ID', async function () {
-        const req = { params: { id: 1 } };
-        const res = {};
+  it('Recuperar um produto pelo ID', async function () {
+      const req = { params: { id: 1 } };
+      const res = {};
 
-        res.status = sinon.stub().returns(res);
-        res.json = sinon.stub().returns(res);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
 
-        sinon.stub(productsService, 'findProductById')
-          .resolves({ type: 200, data: allProducts[0] });
+      sinon.stub(productsService, 'findProductById')
+        .resolves({ type: 200, data: allProducts[0] });
 
-        await productsController.findProductById(req, res);
+      await productsController.findProductById(req, res);
 
-        expect(res.status).to.have.been.calledWith(200);
-        expect(res.json).to.have.been.calledWith(allProducts[0]);
-    });
-    
-    it('Procurar por um Id inexistente', async function () {
-        const req = { params: { id: 171 } };
-        const res = {};
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(allProducts[0]);
+  });
+  
+  it('Procurar por um Id inexistente', async function () {
+      const req = { params: { id: 171 } };
+      const res = {};
 
-        res.status = sinon.stub().returns(res);
-        res.json = sinon.stub().returns(res);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
 
-        sinon.stub(productsService, 'findProductById')
-          .resolves({ type: 404, data: { message: 'Product not found' } });
-        
-        await productsController.findProductById(req, res);
+      sinon.stub(productsService, 'findProductById')
+        .resolves({ type: 404, data: { message: 'Product not found' } });
+      
+      await productsController.findProductById(req, res);
 
-        expect(res.status).to.have.been.calledWith(404);
-        expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
-    });
+      expect(res.status).to.have.been.calledWith(404);
+      expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+  });
 
-    it('Criando um novo produto', async function () {
-        const req = { body: { name: 'Martelo do Chapolin' } };
-        const res = {};
+  it('Criando um novo produto', async function () {
+      const req = { body: { name: 'Martelo do Chapolin' } };
+      const res = {};
 
-        res.status = sinon.stub().returns(res);
-        res.json = sinon.stub().returns(res);
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
 
-        sinon.stub(productsService, 'createProduct').resolves({ type: 201, data: newProduct });
+      sinon.stub(productsService, 'createProduct').resolves({ type: 201, data: newProduct });
 
-        await productsController.createProduct(req, res);
+      await productsController.createProduct(req, res);
 
-        expect(res.status).to.have.been.calledWith(201);
-        expect(res.json).to.have.been.calledWith(newProduct);
-    });
+      expect(res.status).to.have.been.calledWith(201);
+      expect(res.json).to.have.been.calledWith(newProduct);
+  });
+
+  it('Criando um produto sem nome', async function () {
+      const req = { body: { name: '' } };
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+
+      sinon.stub(productsService, 'createProduct')
+        .resolves({ type: 400, data: { message: '"name" is required' } });
+
+      await productsController.createProduct(req, res);
+
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.json).to.have.been.calledWith({ message: '"name" is required' });
+  });
+
+  it('Criando um produto com tamanho do name menor que 5', async function () {
+    const req = { body: { name: 'Bob' } };
+    const res = {};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+
+    sinon.stub(productsService, 'createProduct')
+    .resolves({ type: 422, data: { message: '"name" length must be at least 5 characters long' } });
+
+    await productsController.createProduct(req, res);
+
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been
+    .calledWith({ message: '"name" length must be at least 5 characters long' });
+});
 });
